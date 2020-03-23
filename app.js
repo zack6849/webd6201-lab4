cea30e5e059fd981bd5e169917a3d6a98a30b19d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-03-23 19:02:36 -0400

    added content pages to web app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-// controllers
+// route controllers
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 let express = require('express');
+
+// like a private variable
+let myVariable = "WEBD6201";
+
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', 
-  { title: 'Express' 
+  { 
+    title: 'Welcome to WEBD6201',
+    nameString: myVariable
   }
   );
 });
