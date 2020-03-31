008984214ec5d2e50e28966289fa7907a9e4cc73
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-03-31 15:23:18 -0400

    restructured application

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-let app = require('./app');
+let app = require('./server/config/app');
 let debug = require('debug')('webd6201-w2020-lesson9:server');
 let http = require('http');
 
--- server/config/app.js
+++ server/config/app.js
@@ -5,20 +5,20 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 // route controllers
-let indexRouter = require('./routes/index');
+let indexRouter = require('../routes/index');
 
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use('/', indexRouter);
 
